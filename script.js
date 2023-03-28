9c09f2188e712a37dc8fff82ed32a2db3c3f9a23
asmallick1999 <asmallick1999@gmail.com>
2023-03-29 01:46:13 +0530

    add practice code

---
+++ script.js
@@ -0,0 +1,18 @@
+
+    // React 
+    const heading1 = React.createElement("h2",{
+        id:"Heading2"
+    },"Gudmaranir Bhai")
+
+    const main = ReactDOM.createRoot(document.getElementById("main"));
+    main.render(heading1);
+
+
+
+
+    // Vanilla JS 
+    // const heading1 = document.createElement("h1")
+
+    // heading1.innerHTML = 'hello javascript'
+
+    // document.getElementById("main").appendChild(heading1)
